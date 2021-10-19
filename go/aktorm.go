103929c80cae394ddf2d0fb2dd94a059c5f323a7
akitasummer <644171127@qq.com>
2021-10-19 23:43:40 +0800

    feat: init go

---
+++ go/aktorm.go
@@ -0,0 +1,38 @@
+package aktorm
+
+import (
+	"database/sql"
+
+	"aktorm/log"
+	"aktorm/session"
+)
+
+type Engine struct {
+	db *sql.DB
+}
+
+func NewEngine(driver, source string) (e *Engine, err error) {
+	db, err := sql.Open(driver, source)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	if err = db.Ping(); err != nil {
+		log.Error(err)
+		return
+	}
+	e = &Engine{db: db}
+	log.Info("Connect database success")
+	return
+}
+
+func (engine *Engine) Close() {
+	if err := engine.db.Close(); err != nil {
+		log.Error("Failed to close database")
+	}
+	log.Info("Close database success")
+}
+
+func (engine *Engine) NewSession() *session.Session {
+	return session.New(engine.db)
+}
