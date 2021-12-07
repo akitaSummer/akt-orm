c4774ba2b5be48659c46bd0bf96aa6c89f9a5cbc
akitaSummer <644171127@qq.com>
2021-12-07 22:46:45 +0800

    feat: add chain

--- go/session/record_test.go
+++ go/session/record_test.go
@@ -20,18 +20,32 @@ func testRecordInit(t *testing.T) *Session {
 	return s
 }
 
-func TestSession_Insert(t *testing.T) {
+func TestSession_Limit(t *testing.T) {
 	s := testRecordInit(t)
-	affected, err := s.Insert(user3)
-	if err != nil || affected != 1 {
-		t.Fatal("failed to create record")
+	var users []User
+	err := s.Limit(1).Find(&users)
+	if err != nil || len(users) != 1 {
+		t.Fatal("failed to query with limit condition")
 	}
 }
 
-func TestSession_Find(t *testing.T) {
+func TestSession_Update(t *testing.T) {
 	s := testRecordInit(t)
-	var users []User
-	if err := s.Find(&users); err != nil || len(users) != 2 {
-		t.Fatal("failed to query all")
+	affected, _ := s.Where("Name = ?", "Tom").Update("Age", 30)
+	u := &User{}
+	_ = s.OrderBy("Age DESC").First(u)
+
+	if affected != 1 || u.Age != 30 {
+		t.Fatal("failed to update")
+	}
+}
+
+func TestSession_DeleteAndCount(t *testing.T) {
+	s := testRecordInit(t)
+	affected, _ := s.Where("Name = ?", "Tom").Delete()
+	count, _ := s.Count()
+
+	if affected != 1 || count != 1 {
+		t.Fatal("failed to delete or count")
 	}
 }
