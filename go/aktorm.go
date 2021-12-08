a9103e3db48d3faaf200d6c18607044f9c602579
akitaSummer <644171127@qq.com>
2021-12-08 23:08:38 +0800

    feat: add migrate

--- go/aktorm.go
+++ go/aktorm.go
@@ -6,6 +6,8 @@ import (
 	"aktorm/dialect"
 	"aktorm/log"
 	"aktorm/session"
+	"fmt"
+	"strings"
 )
 
 type Engine struct {
@@ -63,4 +65,59 @@ func (engine *Engine) Transaction(f TxFunc) (result interface{}, err error) {
 	}()
 
 	return f(s)
+}
+
+func difference(a []string, b []string) []string {
+	diff := []string{}
+	diffMap := map[string]bool{}
+	for _, v := range b {
+		diffMap[v] = true
+	}
+	for _, v := range a {
+		if _, ok := diffMap[v]; !ok {
+			diff = append(diff, v)
+		}
+	}
+	return diff
+}
+
+// ALTER TABLE table_name ADD COLUMN col_name, col_type;
+
+// CREATE TABLE new_table AS SELECT col1, col2, ... from old_table
+// DROP TABLE old_table
+// ALTER TABLE new_table RENAME TO old_table;
+
+func (engine *Engine) Migrate(value interface{}) error {
+	_, err := engine.Transaction(func(s *session.Session) (result interface{}, err error) {
+		if !s.Model(value).HasTable() {
+			log.Infof("table %s doesn't exist", s.RefTable().Name)
+			return nil, s.CreateTable()
+		}
+		table := s.RefTable()
+		rows, _ := s.Raw(fmt.Sprintf("SELECT * FROM %s LIMIT 1", table.Name)).QueryRows()
+		columns, _ := rows.Columns()
+		addCols := difference(table.FieldNames, columns)
+		delCols := difference(columns, table.FieldNames)
+		log.Infof("added cols %v, deleted cols %v", addCols, delCols)
+
+		for _, col := range addCols {
+			f := table.GetField(col)
+			sqlStr := fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s %s;", table.Name, f.Name, f.Type)
+			if _, err = s.Raw(sqlStr).Exec(); err != nil {
+				return
+			}
+		}
+
+		if len(delCols) == 0 {
+			return
+		}
+		tmp := "tmp_" + table.Name
+		fieldStr := strings.Join(table.FieldNames, ", ")
+		s.Raw(fmt.Sprintf("CREATE TABLE %s AS SELECT %s from %s;", tmp, fieldStr, table.Name))
+		s.Raw(fmt.Sprintf("DROP TABLE %s;", table.Name))
+		s.Raw(fmt.Sprintf("ALTER TABLE %s RENAME TO %s;", tmp, table.Name))
+		_, err = s.Exec()
+		return
+	})
+	return err
 }
--- go/aktorm_test.go
+++ go/aktorm_test.go
@@ -3,6 +3,7 @@ package aktorm
 import (
 	"aktorm/session"
 	"errors"
+	"reflect"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -61,4 +62,20 @@ func TestEngine_Transaction(t *testing.T) {
 	t.Run("commit", func(t *testing.T) {
 		transactionCommit(t)
 	})
+}
+
+func TestEngine_Migrate(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+	s := engine.NewSession()
+	_, _ = s.Raw("DROP TABLE IF EXISTS User;").Exec()
+	_, _ = s.Raw("CREATE TABLE User(Name text PRIMARY KEY, XXX integer);").Exec()
+	_, _ = s.Raw("INSERT INTO User(`Name`) values (?), (?)", "Tom", "Sam").Exec()
+	engine.Migrate(&User{})
+
+	rows, _ := s.Raw("SELECT * FROM User").QueryRows()
+	columns, _ := rows.Columns()
+	if !reflect.DeepEqual(columns, []string{"Name", "Age"}) {
+		t.Fatal("Failed to migrate table User, got columns", columns)
+	}
 }
